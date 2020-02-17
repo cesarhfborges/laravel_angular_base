566fba911916e1811efdb8e784f472dad6a55c58
Cesar Henrique <cesar_silk321@hotmail.com>
2020-02-17 20:33:08 -0300

    Cadastro Pronto

---
+++ backend/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'nome' => 'Administrador',
+            'sobrenome' => 'System',
+            'email' => 'cesarhfborges@gmail.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => \Carbon\Carbon::now()
+        ]);
+        DB::table('users')->insert([
+            'nome' => Str::random(10),
+            'sobrenome' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => \Carbon\Carbon::now()
+        ]);
+    }
+}
