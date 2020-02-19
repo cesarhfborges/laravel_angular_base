79645ac0b7208d79788324febc989d47b792e410
Cesar Henrique <cesar_silk321@hotmail.com>
2020-02-18 23:43:36 -0300

    Ajustando prime

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule, PanelModule, SidebarModule} from "primeng";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    PanelModule,
+    ButtonModule,
+    SidebarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/sidemenu/sidemenu.component.ts
@@ -0,0 +1,19 @@
+import {Component, NgModule} from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: []
+})
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.scss'],
+})
+
+export class SidemenuComponent {
+
+  constructor() { }
+
+}
